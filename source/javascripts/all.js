b0ef4c63dfd645695b023ee8eca7f0faa651bdf6
Lawrence Lin <deduce@gmail.com>
2011-12-19 15:16:47 +0800

    JS files added

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -4,4 +4,10 @@ $(function(){
   $('#container').masonry({
     itemSelector : '.item'
   });
+
+  $("#albums-list").live("change", function(e) {
+    load_photos(this.value);
+    location.hash = "#" + this.value;
+  });
+
 });
